86f5b46884a7090d82d6a8581f6b9b1a0d80598b
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-01-24 20:52:18 +0100

    refacto mineur

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -1,14 +1,11 @@
 import { Typography, useTheme } from '@mui/material';
-import { Link } from 'react-router-dom';
 
 const Logo = () => {
   const theme = useTheme();
 
   return (
     <Typography fontWeight="700" fontSize="1.7rem">
-      <Link to="/" textDecoration="none">
-        Kev<span style={{ color: theme.palette.primary.main }}>Flix</span>
-      </Link>
+      Kev<span style={{ color: theme.palette.primary.main }}>Flix</span>
     </Typography>
   );
 };
