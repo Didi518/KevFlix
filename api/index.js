ecfe872ce7406786882501a8270d29771a3fa5c4
Didi518 <kevin.ouali@gmail.com>
2026-05-04 20:02:58 +0200

    maj cors

--- api/index.js
+++ api/index.js
@@ -8,7 +8,12 @@ import routes from './src/routes/index.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://kevflix-mu.vercel.app',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
